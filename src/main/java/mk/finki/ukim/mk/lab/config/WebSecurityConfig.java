21e619c2a49da8db15c0ff79fdcf79c76399020e
Tanas10 <adespotovski09@gmail.com>
2025-03-09 02:01:19 +0100

    Adding security

---
+++ src/main/java/mk/finki/ukim/mk/lab/config/WebSecurityConfig.java
@@ -0,0 +1,77 @@
+package mk.finki.ukim.mk.lab.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebSecurityConfig {
+
+    private final PasswordEncoder passwordEncoder;
+
+
+    public WebSecurityConfig(PasswordEncoder passwordEncoder ) {
+        this.passwordEncoder = passwordEncoder;
+
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers((headers) -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                )
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/api/books/**","/swagger-ui/**", "/v3/api-docs/**").hasAnyRole("LIBRARIAN", "ADMIN")
+                        .anyRequest()
+                        .hasRole("ADMIN")
+                )
+                .formLogin((form) -> form
+                        .permitAll()
+                        .failureUrl("/login?error=BadCredentials")
+
+                )
+                .logout((logout) -> logout
+                        .logoutUrl("/logout")
+                        .clearAuthentication(true)
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                        .logoutSuccessUrl("/login")
+                );
+
+        return http.build();
+    }
+
+    // In Memory Authentication
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails LIBRARIAN = User.builder()
+                .username("librarian")
+                .password(passwordEncoder.encode("li"))
+                .roles("LIBRARIAN")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(LIBRARIAN, admin);
+    }
+
+
+}
